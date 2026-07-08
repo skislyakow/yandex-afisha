d61665b4ef9a3aabefce7f40c9ab9a115a12625d
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-07-08 13:46:07 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,3 +1,47 @@
+import json
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Place
 
-# Create your views here.
+
+def start(request):
+    places = Place.objects.all()
+
+    features = []
+    for place in places:
+        features.append(
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [place.lng, place.lat],
+                },
+                "properties": {
+                    "title": place.title,
+                    "placeId": place.pk,
+                    "detailsUrl": f"/places/{place.pk}",
+                },
+            }
+        )
+
+    geojson = {
+        "type": "FeatureCollection",
+        "features": features,
+    }
+
+    return render(request, "index.html", {"places_geojson": geojson})
+
+
+def place_detail(request, pk):
+    place = get_object_or_404(Place, pk=pk)
+    images = place.images.all()
+
+    data = {
+        "title": place.title,
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "imgs": [image.image.url for image in images],
+    }
+
+    return JsonResponse(data)
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -19,10 +19,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import TemplateView
+
+from places.views import start, place_detail
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="index.html"), name="start"),
+    path("", start, name="start"),
+    path("places/<int:pk>/", place_detail, name="place_detail"),
     path("admin/", admin.site.urls),
 ]
 
