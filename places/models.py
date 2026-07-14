daf72c22159e3b181ce583673dcca69e713bbb9d
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-07-14 10:29:45 +0300

    add russian verbose_name definition

--- places/models.py
+++ places/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=200)
-    description_short = models.TextField()
-    description_long = models.TextField()
-    lng = models.FloatField()
-    lat = models.FloatField()
+    title = models.CharField("Название", max_length=200)
+    description_short = models.TextField("Краткое описание")
+    description_long = models.TextField("Полное описание")
+    lng = models.FloatField("Долгота")
+    lat = models.FloatField("Широта")
+
+    class Meta:
+        verbose_name = "Место"
+        verbose_name_plural = "Места"
 
     def __str__(self) -> str:
         return self.title
@@ -33,12 +37,15 @@ class PlaceImage(models.Model):
         Place,
         on_delete=models.CASCADE,
         related_name="images",
+        verbose_name="Место",
     )
-    image = models.ImageField(upload_to="places_images")
-    ordering = models.PositiveIntegerField(default=0)
+    image = models.ImageField("Изображение", upload_to="places_images")
+    ordering = models.PositiveIntegerField("Порядок", default=0)
 
     class Meta:
         ordering = ["ordering"]
+        verbose_name = "Изображение места"
+        verbose_name_plural = "Изображение мест"
 
     def __str__(self) -> str:
         return f"{self.place.title} - картинка {self.ordering}"
