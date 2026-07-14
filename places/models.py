6d5913b138992ced19b211bf8da45bac2c399fa1
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-07-14 10:55:50 +0300

    add blank and default definition to model

--- places/models.py
+++ places/models.py
@@ -3,8 +3,12 @@ from django.db import models
 
 class Place(models.Model):
     title = models.CharField("Название", max_length=200)
-    description_short = models.TextField("Краткое описание")
-    description_long = models.TextField("Полное описание")
+    description_short = models.TextField(
+        "Краткое описание", blank=True, default=""
+    )
+    description_long = models.TextField(
+        "Полное описание", blank=True, default=""
+    )
     lng = models.FloatField("Долгота")
     lat = models.FloatField("Широта")
 
